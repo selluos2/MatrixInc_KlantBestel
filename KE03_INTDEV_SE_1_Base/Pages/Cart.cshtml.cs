d2158bfdf026a9e749c8eb117d4ca5605f02e6e4
Mik <mik.penders@gmail.com>
2026-05-17 22:23:34 +0200

    - added cart functionality - cart looks decent now - site has basic functionalities working

--- KE03_INTDEV_SE_1_Base/Pages/Cart.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Cart.cshtml.cs
@@ -10,19 +10,31 @@ namespace KE03_INTDEV_SE_1_Base.Pages
     public class CartModel : PageModel
     {
         private readonly IProductRepository _productRepository;
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IOrderRepository _orderRepository;
         public Cart Cart { get; set; }
         public CartDisplay ItemInCart { get; set; }
+        public Customer? CurrentCustomer { get; set; }
         public List<CartDisplay> Items { get; set; }
+        public decimal Total { get; set; } = 0;
         public Product Product { get; set; }
 
-        public CartModel(IProductRepository productRepository)
+        public CartModel(IProductRepository productRepository, ICustomerRepository customerRepository, IOrderRepository orderRepository)
         {
             _productRepository = productRepository;
+            _customerRepository = customerRepository;
             Items = new List<CartDisplay>();
+            _orderRepository = orderRepository;
         }
 
         public void OnGet()
         {
+            CurrentCustomer = null;
+            SimpleCustomer currentCustomer = HttpContext.Session.GetObject<SimpleCustomer>("Customer");
+            if (currentCustomer != null)
+            {
+                CurrentCustomer = _customerRepository.GetCustomerById(currentCustomer.Id);
+            }
             Cart = HttpContext.Session.GetObject<Cart>("Cart") ?? new Cart();
             if (Cart.Items.Any())
             {
@@ -33,6 +45,35 @@ namespace KE03_INTDEV_SE_1_Base.Pages
                     Items.Add(ItemInCart);
                 }
             }
+            foreach (var item in Items)
+            {
+                Total += item.product.Price * item.quantity;
+            }
+        }
+
+        public IActionResult OnPostOrderCart()
+        {
+            SimpleCustomer currentCustomer = HttpContext.Session.GetObject<SimpleCustomer>("Customer");
+            CurrentCustomer = _customerRepository.GetCustomerById(currentCustomer.Id);
+            Cart = HttpContext.Session.GetObject<Cart>("Cart") ?? new Cart();
+
+            Order order = new Order
+            {
+                CustomerId = CurrentCustomer.Id,
+                OrderDate = DateTime.Now
+            };
+            foreach (var item in Cart.Items)
+            {
+                OrderProduct orderProduct = new OrderProduct
+                {
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity
+                };
+                order.OrderProducts.Add(orderProduct);
+            }
+            _orderRepository.AddOrder(order);
+            HttpContext.Session.SetObject("Cart", new Cart());
+            return RedirectToPage("/cart");
         }
     }
 }
